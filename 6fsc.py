6c77609d767490906f1a9c5abff9ac9599589822
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-05-09 16:18:53 -0300

    Add files via upload

---
+++ 6fsc.py
@@ -0,0 +1,15 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit,
+#transforme e ,mostre a temperatura em graus Celsius.
+#C= 5 * C = 5 * ((F-32) / 9).
+
+f=input ("Qual a temperatura em °F?")
+x=float(f)
+print (x)
+C= 5 * (x-32) /9
+print(C) 
+
+
+
+
+
+
---
+++ 7letra.py
@@ -0,0 +1,5 @@
+letrinha=input("Digite uma letra: \n")
+if letrinha=="a":
+    
+    vogais=["a","e","i","o","u"]
+    print("vogais")
